da35b7c634727e020e6885c47eb010b2028ecdc7
River Kim <bluuminn@gmail.com>
2024-06-17 01:57:35 +0900

    etc: controller advice body 수정

--- src/main/java/antigravity/exception/AntigravityControllerAdvice.java
+++ src/main/java/antigravity/exception/AntigravityControllerAdvice.java
@@ -1,11 +1,13 @@
 package antigravity.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
@@ -14,14 +16,20 @@ public class AntigravityControllerAdvice {
     @ExceptionHandler(AntigravityApplicationException.class)
     public ResponseEntity<?> applicationHandler(AntigravityApplicationException e) {
         log.error("Error occurs: {}", e.toString());
+        Map<String, String> error = new HashMap<>();
+        error.put("errorCode", e.getErrorCode().name());
+        error.put("errorMessage", e.getErrorCode().getMessage());
         return ResponseEntity.status(e.getErrorCode().getStatus())
-                .body(e.getErrorCode().name());
+                .body(error);
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<?> runtimeHandler(RuntimeException e) {
         log.error("Error occurs: {}", e.toString());
-        return ResponseEntity.status(INTERNAL_SERVER_ERROR)
-                .body(INTERNAL_SERVER_ERROR.name());
+        Map<String, String> error = new HashMap<>();
+        error.put("errorCode", HttpStatus.INTERNAL_SERVER_ERROR.name());
+        error.put("errorMessage", e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(HttpStatus.INTERNAL_SERVER_ERROR.name());
     }
 }
